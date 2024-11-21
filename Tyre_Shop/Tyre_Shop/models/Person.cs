164172a9ba8ffa157f1fe3360a66187ee878bdcb
pdqwe2000 <pdqwe2000@gmail.com>
2024-11-21 16:30:50 +0000

    Path Change
    Form Tyre Shop to Tyre_Shop

--- Tyre_Shop/Tyre_Shop/models/Person.cs
+++ Tyre_Shop/Tyre_Shop/models/Person.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Tyre_Shop.source.interfaces;
 
 namespace Tyre_Shop.models
 {
